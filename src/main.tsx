7243d73e87d011fddfbb8c6de31f0c11e1529eb6
Atifmoin19 <atifmoin52@gmail.com>
2026-01-08 16:06:56 +0530

    adde new builds

--- src/main.tsx
+++ src/main.tsx
@@ -12,16 +12,32 @@ import App from "App";
 import { store } from "app/store";
 import { theme } from "Styles";
 
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+
 const persistor = persistStore(store);
 
+function MyEmotionProvider({
+  nonce,
+  children,
+}: Readonly<{
+  nonce: string;
+  children: React.ReactNode;
+}>) {
+  const cache = createCache({ nonce: nonce, key: "css" });
+  return <CacheProvider value={cache}>{children}</CacheProvider>;
+}
+
 createRoot(document.getElementById("root")!).render(
-  <ChakraProvider resetCSS theme={theme}>
-    <Provider store={store}>
-      <Router basename="/task-automation">
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
-      </Router>
-    </Provider>
-  </ChakraProvider>
+  <MyEmotionProvider nonce="bbbdbadfcdbafaffdbfcddccdbfafddb">
+    <ChakraProvider resetCSS theme={theme}>
+      <Provider store={store}>
+        <Router basename="/task-automation">
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
+        </Router>
+      </Provider>
+    </ChakraProvider>
+  </MyEmotionProvider>
 );
