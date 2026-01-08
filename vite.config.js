3417a30f63ec949fa7aefd2785f0672f1f4c5107
Atifmoin19 <atifmoin52@gmail.com>
2026-01-08 16:01:54 +0530

    added new tar

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/task-automation/",
     plugins: [
         react(),
         tsconfigPaths(),
